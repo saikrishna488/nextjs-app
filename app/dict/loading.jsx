a87b023b6ee7cdc8f7caa93a223c1caf708ab5b7
saikrishna488 <20p61a05h0@vbithyd.ac.in>
2023-04-20 15:16:40 +0530

    added dict page

---
+++ app/dict/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='container'>
+      <h2>Loading.....</h2>
+    </div>
+  )
+}
+
+export default loading
---
+++ app/dict/page.jsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import Loading from "./loading";
+
+const page = () => {
+  const [word, setWord] = useState(null);
+  const [meaning, setMeaning] = useState("Enter Word To search");
+  const [loading, setLoading] = useState(false);
+  const fetchData = async (word) => {
+    setLoading(true);
+    let res = await fetch(
+      `https://dictionary-api-chi.vercel.app/dict/${word}`,
+      {
+        cache: "no-cache",
+      }
+    );
+    let data = await res.json();
+    setMeaning(data.meaning ? data.meaning : data.msg);
+    setLoading(false);
+  };
+
+  const onClick = (word) => {
+    if (word == null) {
+      setMeaning("Enter Word To search");
+    } else {
+      fetchData(word);
+    }
+  };
+
+  return (
+    <div className="container">
+      {!loading ? (
+        <>
+          <div className="input-con">
+            <input
+              type="text"
+              className="input"
+              onChange={(e) => setWord(e.target.value)}
+            />
+            <button id="btn" onClick={() => onClick(word)}>
+              Get Meaning
+            </button>
+          </div>
+          <h2>{meaning}</h2>
+        </>
+      ) : (
+        <Loading />
+      )}
+    </div>
+  );
+};
+
+export default page;
